7d4b5ab37ab98fec66e1af457ea2c7ed01df1f2e
hidang <yellowstudio.vn@gmail.com>
2022-06-18 12:00:56 +0700

    config to deploy github page

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const Axios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000", // https://frientor-server-nhom5.herokuapp.com --> for build to deploy production
   headers: {
     "content-type": "application/json",
   },
